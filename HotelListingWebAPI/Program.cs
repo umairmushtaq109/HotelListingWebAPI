235684e8c430dd37fbb7550323130f68d44c6dcf
umairmushtaq109 <71635291+umairmushtaq109@users.noreply.github.com>
2022-09-28 14:29:40 +0500

    Initial commit

---
+++ HotelListingWebAPI/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Added For SeriLog
+var logger = new LoggerConfiguration()
+  .ReadFrom.Configuration(builder.Configuration)
+  .Enrich.FromLogContext()
+  .CreateLogger();
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1"}));
+
+//Added For SeriLog
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
+//Added Cross Origin Resourse Sharing Policy CORS (For Communicating with Different Network/Project)
+builder.Services.AddCors(c => c.AddPolicy(
+    "AllowAll",
+    builder => builder.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader())
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(policyName: "AllowAll");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    logger.Information("Application is Starting");
+    app.Run();
+
+}
+catch (Exception e)
+{
+    logger.Fatal(e, "Application Failed to Start");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
